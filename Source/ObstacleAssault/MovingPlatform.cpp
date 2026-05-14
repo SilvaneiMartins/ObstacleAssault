8ca27400bbe5b4091d64971e5cc5b71fdaa6e53a
Silva Martins <silvaneimartins_rcc@hotmail.com>
2026-05-14 13:38:26 -0300

    feat: functions

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,24 +1,24 @@
-// Preencha o aviso de direitos autorais na página Descrição das Configurações do Projeto.
-
 
 #include "MovingPlatform.h"
 
-// Define valores padrão
 AMovingPlatform::AMovingPlatform()
 {
- 	// Configure este ator para chamar Tick() a cada frame. Você pode desativar essa configuração para melhorar o desempenho, caso não precise dela.
-	PrimaryActorTick.bCanEverTick = true;
+ 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Chamado quando o jogo inicia ou quando o personagem é gerado.
+void MyTestFunction()
+{
+	UE_LOG(LogTemp, Display, TEXT("MyTestFunction is called!!"));
+}
+
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
+	MyTestFunction();
 }
 
-// Chamado de cada quadro
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
